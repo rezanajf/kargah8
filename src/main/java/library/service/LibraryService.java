cc4291c9c04990180fda2c3182aba1f0c6a93a9a
Parsa <Parsa.lens@gmail.com>
2026-06-13 15:46:27 +0330

    Add findBookById method and construstor and fields

--- src/main/java/library/service/LibraryService.java
+++ src/main/java/library/service/LibraryService.java
@@ -1,4 +1,27 @@
 package main.java.library.service;
 
+import main.java.library.model.Book;
+import main.java.library.util.FileManager;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 public class LibraryService {
+
+    private List<Book> books;
+
+    public LibraryService() {
+        this.books = FileManager.loadBooksFromCSV();
+        FileManager.saveBooksToCSV(this.books);
+    }
+
+    public Optional<Book> findBookById(int id) {
+        for (Book book : books) {
+            if (book.getId() == id) {
+                return Optional.of(book);
+            }
+        }
+        return Optional.empty();
+    }
 }
